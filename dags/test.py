48304f6d6cfdb0fd42da679e3bb485bc3e4bc0a2
enggabhishek <abhiemin@gmail.com>
2024-07-21 15:24:20 -0400

    elastic push it again

--- dags/test.py
+++ dags/test.py
@@ -2,15 +2,18 @@ from airflow.decorators import task, dag
 # from datetime import timedelta
 # from airflow import DAG
 from datetime import datetime
-# from airflow.operators.empty import EmptyOperator
-# from elasticsearch import Elasticsearch
+from airflow.operators.empty import EmptyOperator
+from elasticsearch import Elasticsearch
+from airflow.providers.apache.kafka.operators.produce import ProduceToTopicOperator
+from airflow.providers.apache.kafka.operators.consume import ConsumeFromTopicOperator
+import pandas as pd
 import os
-# elasticsearch_CloudID=os.getenv('ELASTIC_CLOUD_ID')
-# elastic_pwd=os.getenv('ELASTIC_PASSWORD')
-# es = Elasticsearch(
-#                 cloud_id = elasticsearch_CloudID,
-#                 basic_auth=("elastic", elastic_pwd)
-#                 )
+elasticsearch_CloudID=os.getenv('ELASTIC_CLOUD_ID')
+elastic_pwd=os.getenv('ELASTIC_PASSWORD')
+es = Elasticsearch(
+                cloud_id = elasticsearch_CloudID,
+                basic_auth=("elastic", elastic_pwd)
+                )
 @dag(start_date=datetime(2024, 6, 29), schedule_interval= None, catchup=False)
 def checking_ti_again_xcom():
     
